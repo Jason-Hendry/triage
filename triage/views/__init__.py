db6873061461f59b19fbf16f4074512c530bd2f6
Jason Hendry <jason@rain.com.au>
2012-11-30 12:12:39 +1100

    Fix 'New Project' and split out edit into seperate method, Allow default project None to point to project list page

--- triage/views/__init__.py
+++ triage/views/__init__.py
@@ -8,5 +8,8 @@ log = logging.getLogger(__name__)
 @view_config(route_name='index')
 def index(request):
     selected_project = request.registry.settings['default_project']
-    url = request.route_url('error_list', project=selected_project)
+    if not selected_project:
+      url = request.route_url('error_projects')
+    else:
+      url = request.route_url('error_list', project=selected_project)
     return HTTPFound(location=url)
